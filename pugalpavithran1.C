64e63308772cd0148ec4059fb56f9fe59b162c0e
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-02 13:25:42 +0530

    pugalpavithran1.C

---
+++ pugalpavithran1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number",a);
+	scanf("%d",&a);
+	if(a<0)
+	printf("the number is postive");
+	else
+	printf("the number is nagative");
+	return 0;
+}
