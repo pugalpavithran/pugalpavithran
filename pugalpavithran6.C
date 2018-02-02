ed8ecec9a0868f0b1b227d81a8fc192745ff7e5e
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-02 13:43:49 +0530

    pugalpavithran6.C

---
+++ pugalpavithran6.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number",a);
+	scanf("%d",&a);
+	if(a<2016)
+	printf("the year is a leap");
+	else
+	printf("the year is a not");
+	return 0;
+}
