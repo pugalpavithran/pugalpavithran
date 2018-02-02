4d6d32f92fd3c1344cf76c11cbfe2077797817cc
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-02 13:32:35 +0530

    pugalpavithran4.C

---
+++ pugalpavithran4.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number",a);
+	scanf("%d",&a);
+	if(a<0)
+	printf("the number is alphbet");
+	else
+	printf("the number is no");
+	return 0;
+}
