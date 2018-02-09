b876909170eac86beb69149b73fbaead52ddbf3e
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-09 13:31:33 +0530

    Update pugalpavithran5.C

--- pugalpavithran5.C
+++ pugalpavithran5.C
@@ -1,6 +1,6 @@
 #include <stdio.h>
  
-void main()
+int main()
 {
     int num1, num2, num3;
  
@@ -8,17 +8,23 @@ void main()
     scanf("%d %d %d", &num1, &num2, &num3);
     printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
     if (num1 > num2)
+     
+     
     {
         if (num1 > num3)
         {
             printf("num1 is the greatest among three \n");
         }
         else
+         
+         
         {
             printf("num3 is the greatest among three \n");
         }
     }
     else if (num2 > num3)
+     
+     
         printf("num2 is the greatest among three \n");
     else
         printf("num3 is the greatest among three \n");
