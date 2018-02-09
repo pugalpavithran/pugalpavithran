c77d6c35dfe1b0e443b208074e252746b47fe346
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-09 13:36:14 +0530

    Update pugalpavithran8.C

--- pugalpavithran8.C
+++ pugalpavithran8.C
@@ -1,17 +1,20 @@
  
 #include<stdio.h>
-int main()
+void main()
 {
   int Number, i, Sum = 0;
   
   printf("\nPlease Enter any Integer Value\n");
+ 
   scanf("%d", &Number);
   
   for(i = 1; i <= Number; i++)
   {
      Sum = Sum + i;
+   
   }
   
   printf("Sum of Natural Numbers = %d", Sum);
+ 
   return 0;
 }
