c85f41d70a9f7b076c2083d66a423b84ac2a02e9
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-09 13:55:23 +0530

    Update pavi12-09.C

--- pavi12-09.C
+++ pavi12-09.C
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(void)
+void main()
 {
 int n,reverse=0,temp;
 {
@@ -8,13 +8,16 @@ scanf("%d",&n);
 {
 temp=n;
 while(temp!=0)
+  
 {
 reverse=reverse*10;
 reverse=reverse+temp%10;
+  
 temp=temp/10;
 }
 if(temp=reverse)
 printf("%d is a palindrome",n);
+  
 else
 printf("%d is not an palindrome",n);
 }
