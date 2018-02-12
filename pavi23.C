f40e80ea14d430634b206334510fe77fed2fa785
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-12 16:11:09 +0530

    Create pavi23.C

---
+++ pavi23.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int a[50],n,i,min;
+	printf("enter no of elements:");
+  
+	scanf("%d",&n);
+  
+	for(i=0;i<n;i++)
+  
+	{
+		scanf("%d",&a[i]);
+   }
+     min = a[0];
+ 
+  for (i = 1; i < n; i++)
+  {
+    if (a[i] < min)
+    
+    {
+       min = a[i];
+      
+    }
+  }
+ 
+  printf("%d",min);
+  
+
+return 0;	
+}
