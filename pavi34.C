bd06ac14f54e82105aa0b787e12c96d0dfab1ddf
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-21 16:00:20 +0530

    Create pavi34.C

---
+++ pavi34.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ char a[30];
+ int i, count=0;
+ printf("enter a paragraph");
+ 
+ scanf("%[^\n]s",&a);
+ 
+ for(i=0;a[i]!='\0';i++)
+ 
+ 
+ {
+ if(a[i]=='.')
+ 
+ {
+ count=count+1;
+ 
+ }
+ }
+ printf("\nnumber of lines=%d",count);
+ 
+return 0;
+}
