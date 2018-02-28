2495fefd981b1599b678ce385ef70027431bee37
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-28 15:58:39 +0530

    Create pavi36.C

---
+++ pavi36.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+    int main()
+{
+    char    a[100];
+    int b=0,i;
+    scanf("%[^\n]s",a);
+ for(i=0;a[i]!=NULL;i++)
+    {
+ 
+        if(a[i]>='0' && a[i]<='9')
+        {
+        	b++;
+        }
+       
+    }
+      printf("%d",b);
+    
+ 
+    return 0;
+}
