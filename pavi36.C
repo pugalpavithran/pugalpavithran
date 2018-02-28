8e83dfe5dd9bd51785b6f0fdf6877b77517f26d2
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-02-28 16:01:38 +0530

    Update pavi36.C

--- pavi36.C
+++ pavi36.C
@@ -4,16 +4,20 @@
     char    a[100];
     int b=0,i;
     scanf("%[^\n]s",a);
+        
  for(i=0;a[i]!=NULL;i++)
+     
     {
  
         if(a[i]>='0' && a[i]<='9')
+            
         {
         	b++;
         }
        
     }
       printf("%d",b);
+        
     
  
     return 0;
