899b2db1492c6703943fc8b9a10fc7262f37c4fc
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-03-02 13:20:40 +0530

    Create pavi41.C

---
+++ pavi41.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int i,k;
+  
+	char s[100]; 
+  
+	printf("Enter the String:");
+  
+	scanf("%s",&s);
+  
+	scanf("%d",&k);
+  
+	for(i=0;i<k;i++)
+  
+	{
+		printf("%s\n",s);
+    
+	}
+}
