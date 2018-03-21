d8b995ddc38a907eff9cb8460c9670e095e3d97d
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-03-21 16:22:08 +0530

    Create pavi73.C

---
+++ pavi73.C
@@ -0,0 +1,22 @@
+int main() 
+{
+	int n,l,r;
+	printf("\n enter the number:");
+  
+	scanf("%d",&n);
+  
+	printf("\n enter the left and right range:");
+  
+	scanf("%d %d",&l,&r);
+  
+	if(n>l && n<r)
+  
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
