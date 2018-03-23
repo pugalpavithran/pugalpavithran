b71c7a1a8e12df09c487f455936d8eac49891dd6
pugalpavithran <35764721+pugalpavithran@users.noreply.github.com>
2018-03-23 15:45:26 +0530

    Create pavi78.C

---
+++ pavi78.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n;
+	printf("enter the number:");
+
+	scanf("%d",&n);
+	if(n%13==0)
+
+	{
+		printf("\n yes");
+
+	}
+	else
+
+	{
+		printf("\n no");
+
+	}
+	return 0;
+
+}
